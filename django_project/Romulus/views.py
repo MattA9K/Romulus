6370a56d6cfbddb5c9ec5511791e0bff9ed505d4
Matt Andrzejczuk <CiniCraft@Matts-Mac-Pro-2.local>
2015-04-26 21:17:17 -0700

    small change

--- django_project/Romulus/views.py
+++ django_project/Romulus/views.py
@@ -1,6 +1,10 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 # Create your views here.
 
 def index(request):
     return HttpResponse("<h1>Project Romulus</h1><br><p>under construction...</p>")
+
+def html(request):
+    return render_to_response('index.html')
