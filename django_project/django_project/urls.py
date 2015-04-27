420e72551f05d8eddbbbb0d0cfcf1b9a841cb3de
Matt Andrzejczuk <CiniCraft@Matts-Mac-Pro-2.local>
2015-04-26 20:59:04 -0700

    small change

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^Romulus/', include('Romulus.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
