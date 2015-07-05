8c7c8dc3333feb88d1b126666f412202a6afc4e7
Matt Andrzejczuk <CiniCraft@Matts-iMac.local>
2015-07-04 17:36:27 -0700

    almost ready to finalize

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^Romulus/', include('Romulus.urls')),
     url(r'^test', 'Romulus.views.test'),
     url(r'^BrandiLove', 'Romulus.views.index'),
+    url(r'^Secret', 'Romulus.views.index'),
     url(r'^$', 'Romulus.views.authorize_access'),
 
     url(r'^admin/', include(admin.site.urls)),
