3b41195e0dd21429a693f81dfb7c75a6c36899e5
Matt Andrzejczuk <CiniCraft@Matts-iMac.local>
2015-07-12 22:32:19 -0700

    created models and some views

--- django_project/Romulus/views.py
+++ django_project/Romulus/views.py
@@ -8,14 +8,31 @@ from Romulus.models import *
 def test(request):
     return HttpResponse("<h1>Project Romulus</h1><br><p>under construction...</p>")
 
+
 def index(request):
-    hit = PageHit(remote_address=request.META['REMOTE_ADDR'], user_agent=request.META['HTTP_USER_AGENT'], referer=request.META['HTTP_REFERER'])
+    hit = PageHit()
+    try:
+        hit.remote_address=request.META['REMOTE_ADDR']
+    except:
+        hit.remote_address='✖'
+
+    try:
+        hit.user_agent=request.META['HTTP_USER_AGENT']
+    except:
+        hit.user_agent='✖'
+
+    try:
+        hit.referer=request.META['HTTP_REFERER']
+    except:
+        hit.referer='✖'
     hit.save()
     return render_to_response('index.html')
+
 
 def json(request):
     json = {"test":"hello"}
     return HttpResponse("<h1>Project Romulus</h1><br><p>under construction...</p>")
+
 
 def authorize_access(request):
     return render_to_response('auth.html')
