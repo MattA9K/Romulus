de0e66ecd7f755705f85da40a5eb5dc7e2ac9bbb
Matt Andrzejczuk <CiniCraft@Matts-iMac.local>
2015-07-12 21:39:59 -0700

    created models and some views

--- django_project/Romulus/models.py
+++ django_project/Romulus/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from time import time
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    Title = models.CharField(max_length=50)
+    Email = models.CharField(max_length=50)
+    Body = models.TextField()
+    birth_date = models.DateTimeField(default=datetime.datetime.now)
+
+class PageHit(models.Model):
+    pagehit_time = models.DateTimeField(default=datetime.datetime.now)
+    remote_address = models.CharField(max_length=150)
+    user_agent = models.CharField(max_length=150)
+    referer = models.CharField(max_length=150)
--- django_project/Romulus/views.py
+++ django_project/Romulus/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from Romulus.models import *
 # Create your views here.
 
 
@@ -8,6 +9,8 @@ def test(request):
     return HttpResponse("<h1>Project Romulus</h1><br><p>under construction...</p>")
 
 def index(request):
+    hit = PageHit(pagehit_time=time.localtime(), remote_address=request.META['REMOTE_ADDR'], user_agent=request.META['HTTP_USER_AGENT'], referer=request.META['HTTP_REFERER'])
+    hit.save()
     return render_to_response('index.html')
 
 def json(request):
